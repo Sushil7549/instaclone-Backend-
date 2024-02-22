fc85b175dfffd84e3782f1133400ca255a782513
SUSHIL KUMAR PRAMANIK <101800300+Sushil7549@users.noreply.github.com>
2024-02-23 00:49:42 +0530

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const plm = require("passport-local-mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/instainsta");
+mongoose.connect("mongodb+srv://sushil970:Sushil7549@cluster0.00upii1.mongodb.net/instaclone" ,{ useUnifiedTopology: true });
 
 const userSchema = mongoose.Schema({
   username: String,
